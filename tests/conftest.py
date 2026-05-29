aa3e2a44f67740be34d387b8ea8ee5297c6f08e9
Griogorii <grigoriishneer88@gmail.com>
2026-05-29 20:44:43 +0300

    added playwright
    added playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright, Page, Playwright
 
 
 @pytest.fixture
-def chromium_page() -> Page:
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        page = browser.new_page()
-        yield page
-        browser.close()
+def chromium_page(playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    yield page
+    browser.close()
