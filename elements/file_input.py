e3e12ecdc6cf5562f474510c4d47381bbef5927a
Griogorii <grigoriishneer88@gmail.com>
2026-06-18 12:13:31 +0300

    added tracing

--- elements/file_input.py
+++ elements/file_input.py
@@ -5,6 +5,6 @@ class Input(BaseElement):
     def type_of(self):
         return 'fileinput'
     def set_input_files(self, file:str, nth:int = 0, **kwargs):
-        with allure.step(f'Set input file "{file}" to "{self.type_of} with name "{self.name}"'):
+        with allure.step(f'Set input file "{file}" to "{self.type_of()} with name "{self.name}"'):
             locator = self.get_locator(nth, **kwargs)
             locator.set_input_files(file)
--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,15 +1,19 @@
+import allure
 import pytest
 from playwright.sync_api import sync_playwright, Page, Playwright
-
 from pages.authentication.registration_page import RegistrationPage
-
+from _pytest.fixtures import SubRequest
 
 @pytest.fixture
-def chromium_page(playwright) -> Page:
+def chromium_page(request: SubRequest, playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
-    page = browser.new_page()
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+    page = context.new_page()
     yield page
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
     browser.close()
+    allure.attach.file(f'./tracing/{request.node.name}.zip', name="trace", attachment_type=allure.attachment_type.ZIP)
 
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright:Playwright):
@@ -23,9 +27,12 @@ def initialize_browser_state(playwright:Playwright):
     context.storage_state(path = 'browser-state.json')
 
 @pytest.fixture
-def chromium_page_with_state(playwright:Playwright):
+def chromium_page_with_state(request: SubRequest, playwright:Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
     yield page
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
     browser.close()
+    allure.attach.file(f'./tracing/{request.node.name}.zip', name="trace", attachment_type=allure.attachment_type.ZIP)
