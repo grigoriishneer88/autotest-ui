f54d47f533319f5647f37d45e18286cf46f1f2f8
Griogorii <grigoriishneer88@gmail.com>
2026-06-26 15:50:58 +0300

    added git ignore

--- fixtures/allure.py
+++ fixtures/allure.py
@@ -5,6 +5,5 @@ from tools.envinronment import create_allure_environment_file
 
 @pytest.fixture(scope='session', autouse=True)
 def save_allure_environment_file():
-
     yield
     create_allure_environment_file()
--- tools/envinronment.py
+++ tools/envinronment.py
@@ -1,4 +1,6 @@
 import os
+import platform
+import sys
 
 from config import settings
 from pages.dashboard import dashboard_page
@@ -6,6 +8,10 @@ from pages.dashboard import dashboard_page
 
 def create_allure_environment_file():
     items = [f'{key}={value}'for key, value in settings.model_dump().items() ]
+    os_info = f'{platform.system()}, {platform.release()}'
+    python_version = sys.version
+    items.append(os_info)
+    items.append(python_version)
     properties = '\n'.join(items)
     with open(settings.allure_results_dir.joinpath('environment.properties'),'w+') as file:
         file.write(properties)
